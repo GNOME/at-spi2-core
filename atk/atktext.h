55e388884f46136248f6ef1b2c262f1c044c5726
Brian Cameron <bcameron@src.gnome.org>
2001-06-28 10:11:00 +0000

    added comment.

--- atk/atktext.h
+++ atk/atktext.h
@@ -140,10 +140,15 @@ struct _AtkTextIface
 						   gint		    end_offset);
   gboolean       (* set_caret_offset)             (AtkText          *text,
                                                    gint             offset);
+
+  /*
+   * signal handlers
+   */
   void		 (* text_changed)                 (AtkText          *text);
   void           (* caret_changed)                (AtkText          *text,
                                                    gint             location);
 };
+
 GType            atk_text_get_type (void);
 
 
