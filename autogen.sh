df9642557a59b72ebe0525005b811d7d3e992393
Havoc Pennington <hp@pobox.com>
2002-08-18 22:21:32 +0000

    hardcode aclocal-1.4/automake-1.4 so that users with both automake 1.6 and
    2002-08-18  Havoc Pennington  <hp@pobox.com>
    
    	* autogen.sh: hardcode aclocal-1.4/automake-1.4 so that users with
    	both automake 1.6 and 1.4 installed get the right automake. Means
    	compilation from CVS will now require the latest automake 1.4
    	release, or manually creating symlinks called "automake-1.4" and
    	"aclocal-1.4"

--- autogen.sh
+++ autogen.sh
@@ -38,7 +38,7 @@ fi
 }
 
 have_automake=false
-if automake --version < /dev/null > /dev/null 2>&1 ; then
+if automake-1.4 --version < /dev/null > /dev/null 2>&1 ; then
 	automake_version=`automake --version | grep 'automake (GNU automake)' | sed 's/^[^0-9]*\(.*\)/\1/'`
 	case $automake_version in
 	   1.2*|1.3*|1.4) 
@@ -72,9 +72,9 @@ if test -z "$AUTOGEN_SUBDIR_MODE"; then
         fi
 fi
 
-aclocal $ACLOCAL_FLAGS
+aclocal-1.4 $ACLOCAL_FLAGS
 
-automake -a $am_opt
+automake-1.4 -a $am_opt
 autoconf
 cd $ORIGDIR
 
