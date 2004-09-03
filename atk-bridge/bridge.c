5694c35edb56bb605fa38c906221bd1e1e1d65c0
billh <billh@e2bd861d-eb25-0410-b326-f6ed22b6b98c>
2004-09-03 11:54:59 +0000

    Fixed C99isms in atk-bridge/bridge.c, cspi/bonobo/cspi-bonobo.c. Patch from Jens Granseuer.
    git-svn-id: http://svn.gnome.org/svn/at-spi/trunk@700 e2bd861d-eb25-0410-b326-f6ed22b6b98c

--- atk-bridge/bridge.c
+++ atk-bridge/bridge.c
@@ -287,9 +287,10 @@ spi_display_name (void)
 		canonical_display_name = ":0";
 	    else
 	    {
+		gchar *display_p, *screen_p;
 		canonical_display_name = g_strdup (display_env);
-		gchar *display_p = strrchr (canonical_display_name, ':');
-		gchar *screen_p = strrchr (canonical_display_name, '.');
+		display_p = strrchr (canonical_display_name, ':');
+		screen_p = strrchr (canonical_display_name, '.');
 		if (screen_p && display_p && ((guint) screen_p > (guint) display_p))
 		{
 		    *screen_p = '\0';
