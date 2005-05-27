26d75e9c9a6be95435cc9a9c6bd2b71d48afadc6
kmaraas <kmaraas@e2bd861d-eb25-0410-b326-f6ed22b6b98c>
2005-05-27 19:06:21 +0000

    2005-05-27  Kjartan Maraas  <kmaraas@gnome.org>
    	* cspi/bonobo/cspi-bonobo-listener.c:
    	(cspi_event_list_remove_by_cb): Fix the build.
    
    
    git-svn-id: http://svn.gnome.org/svn/at-spi/trunk@756 e2bd861d-eb25-0410-b326-f6ed22b6b98c

--- cspi/bonobo/cspi-bonobo-listener.c
+++ cspi/bonobo/cspi-bonobo-listener.c
@@ -72,7 +72,7 @@ cspi_event_list_remove_by_cb (GList *list, gpointer callback)
       EventHandler *eh = l->data;
       next = l->next;
 
-      if (eh->method == callback)
+      if (eh->cb.method == callback)
       {
         list = g_list_delete_link (list, l);
 	cspi_event_handler_free (eh);
