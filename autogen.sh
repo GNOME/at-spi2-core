8d4ece4d1cb400b0a28f0751d1d8987ccb894da1
Li Yuan <li.yuan@sun.com>
2007-04-11 02:56:35 +0000

    use gnome-autogen.sh to simplify autogen.sh.
    2007-04-11  Li Yuan <li.yuan@sun.com>
    
            * autogen.sh, configure.in: use gnome-autogen.sh to simplify
            autogen.sh.
    
    svn path=/trunk/; revision=1167

--- autogen.sh
+++ autogen.sh
@@ -4,113 +4,12 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-ORIGDIR=`pwd`
-cd $srcdir
-PROJECT=Atk
-TEST_TYPE=-f
-FILE=atk/atk.h
+PKG_NAME="atk"
+REQUIRED_AUTOMAKE_VERSION=1.7
 
-DIE=0
-
-have_libtool=false
-if libtoolize --version < /dev/null > /dev/null 2>&1 ; then
-	libtool_version=`libtoolize --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
-	case $libtool_version in
-	    1.4*|1.5*)
-		have_libtool=true
-		;;
-	esac
-fi
-if $have_libtool ; then : ; else
-	echo
-	echo "You must have libtool 1.4 or 1.5 installed to compile $PROJECT."
-	echo "Install the appropriate package for your distribution,"
-	echo "or get the source tarball at http://ftp.gnu.org/gnu/libtool/"
-	DIE=1
-fi
-
-(gtkdocize --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have gtk-doc installed to compile $PROJECT."
-        echo "Install the appropriate package for your distribution,"
-        echo "or get the source tarball at http://ftp.gnome.org/pub/GNOME/sources/gtk-doc/"
-        DIE=1
-}
-                                                                                
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PROJECT."
-	echo "libtool the appropriate package for your distribution,"
-	echo "or get the source tarball at http://ftp.gnu.org/gnu/autoconf/"
-	DIE=1
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
 }
 
-if automake-1.7 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.7
-    ACLOCAL=aclocal-1.7
-else
-        echo
-        echo "You must have automake 1.7.x installed to compile $PROJECT."
-        echo "Install the appropriate package for your distribution,"
-        echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
-        DIE=1
-fi
-                                                                               
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-test $TEST_TYPE $FILE || {
-	echo "You must run this script in the top-level $PROJECT directory"
-	exit 1
-}
-
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
-        if test -z "$*"; then
-                echo "I am going to run ./configure with no arguments - if you wish "
-                echo "to pass any to it, please specify them on the $0 command line."
-        fi
-fi
-
-
-if test -z "$ACLOCAL_FLAGS"; then
-
-	acdir=`$ACLOCAL --print-ac-dir`
-	m4list="glib-2.0.m4 glib-gettext.m4"
-
-	for file in $m4list
-	do
-          	if [ ! -f "$acdir/$file" ]; then
-			echo "WARNING: aclocal's directory is $acdir, but..."
-			echo "		no file $acdir/$file"
-			echo "		You may see fatal macro warnings below."
-			echo "		If these files are installed in /some/dir, set the ACLOCAL_FLAGS "
-                        echo "		environment variable to \"-I /some/dir\", or install"
-            		echo "		$acdir/$file."
-			echo ""
-		fi
-	done
-fi
-
-rm -rf autom4te.cache
-
-$ACLOCAL $ACLOCAL_FLAGS || exit 1
-
-libtoolize --force || exit $?
-gtkdocize || exit 1
-
-glib-gettextize --force || exit $?
-
-autoheader || exit $?
-
-$AUTOMAKE --add-missing || exit $?
-autoconf || exit $?
-cd $ORIGDIR || exit $?
-
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
-        $srcdir/configure --enable-maintainer-mode $AUTOGEN_CONFIGURE_ARGS "$@" || exit $?
-
-        echo 
-        echo "Now type 'make' to compile $PROJECT."
-fi
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
