a41d247913d13949f259e89e4222753643af04aa
Mark Doffman <mdoff@altair-voyager.(none)>
2009-06-11 15:26:42 +0100

    2009-06-11 Mark Doffman <mark.doffman@codethink.co.uk>         Remove the atk-bridge library as well as the client         libaries pyatspi and cspi.
            Beginning commit in splitting the project into two repositories
            The atk and client code will be located in at-spi2-atk.

--- tests/pyatspi/setvars.sh
+++
@@ -1,6 +0,0 @@
-export PYTHONPATH=../../
-
-export TEST_DATA_DIRECTORY=../data
-export TEST_ATSPI_LIBRARY=../../atk-adaptor/.libs/libspiatk.so
-export TEST_MODULES_DIRECTORY=../apps/.libs
-export TEST_APPLICATION=../apps/test-application
