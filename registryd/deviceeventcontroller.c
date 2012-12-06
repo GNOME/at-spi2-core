c3ec6dc427d1c66e4d42fb82938c4534455b15f2
Mike Gorse <mgorse@suse.com>
2012-12-06 09:50:18 -0600

    Remove the correct datum when clearing a hung process
    When clearing hung processes, we were passing the wrong variable to
    g_list_remove, meaning that the datum would never be removed from the
    list, even though it had been freed, resulting in a crash.

--- registryd/deviceeventcontroller.c
+++ registryd/deviceeventcontroller.c
@@ -1218,7 +1218,7 @@ reset_hung_process_from_ping (DBusPendingCall *pending, void *data)
     if (!strcmp (l->data, data))
     {
       g_free (l->data);
-      hung_processes = g_slist_remove (hung_processes, data);
+      hung_processes = g_slist_remove (hung_processes, l->data);
       break;
     }
   }
