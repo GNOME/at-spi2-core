77f85f7cd9d2cbb6e05cd9139741dbd06b110d0e
Chun-wei Fan <fanchunwei@src.gnome.org>
2016-04-20 18:02:27 +0800

    Visual Studio builds: Generate atk.pc
    ...if Python can be found via $(PythonPath) (x86 builds) or
    $(PythonPathX64) (x64 builds).  This makes it easier for packages that
    requires atk.pc for introspection.

---
+++ build/win32/atkpc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#
+# Utility script to generate .pc files for ATK
+# for Visual Studio builds, to be used for
+# building introspection files
+
+# Author: Fan, Chun-wei
+# Date: April 20, 2016
+
+import os
+import sys
+
+from replace import replace_multi
+from pc_base import BasePCItems
+
+def main(argv):
+    base_pc = BasePCItems()
+    
+    base_pc.setup(argv)
+    pkg_replace_items = {'@GLIB_PACKAGES@': 'gobject-2.0,glib-2.0',
+                         '@ATK_API_VERSION@': '1.0'}
+
+    pkg_replace_items.update(base_pc.base_replace_items)
+
+    # Generate atk.pc
+    replace_multi(base_pc.top_srcdir + '/atk.pc.in',
+                  base_pc.srcdir + '/atk.pc',
+                  pkg_replace_items)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
