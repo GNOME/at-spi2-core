6bec005915c4562a17608b0ea3726c9f6cbffc7c
Federico Mena Quintero <federico@gnome.org>
2022-06-01 13:11:13 -0500

    Remove unused prototype
    Seems to be a cut&paste typo of the prototype just below for _atspi_dbus_handle_event

--- atspi/atspi-event-listener-private.h
+++ atspi/atspi-event-listener-private.h
@@ -32,8 +32,6 @@
 
 G_BEGIN_DECLS
 
-DBusHandlerResult _atspi_dbus_handle_Event (DBusConnection *bus, DBusMessage *message, void *data);
-
 void _atspi_send_event (AtspiEvent *e);
 
 DBusHandlerResult _atspi_dbus_handle_event (DBusConnection *bus, DBusMessage *message, void *data);
