cb437cdefc1be6b383f3f50eaee141be0817e5dd
Federico Mena Quintero <federico@gnome.org>
2022-07-06 14:35:52 -0500

    CI: Install the stock grcov, not my branch
    I told github to delete my accessibility branch when it got merged...
    
    ... it got merged ...
    
    ... it got deleted ...
    
    ... and of course I forgot about it, and now the container image
    couldn't be regenerated because my branch no longer exists :)

--- ci/install-grcov.sh
+++ ci/install-grcov.sh
@@ -2,11 +2,4 @@ source ./ci/env.sh
 
 set -eu
 
-REPOSITORY=https://github.com/federicomenaquintero/grcov
-BRANCH=accessible-html
-
-git clone --depth 1 --branch $BRANCH $REPOSITORY
-cd grcov
-cargo build --release
-
-cp target/release/grcov /usr/local/bin/
+cargo install grcov
