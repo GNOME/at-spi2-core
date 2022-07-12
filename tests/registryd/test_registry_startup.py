b2bc77023ed3e19f6f7483645e2a11952c061de0
Federico Mena Quintero <federico@gnome.org>
2022-07-12 12:36:16 -0500

    Test the Description property of the registry's root

--- tests/registryd/test_registry_startup.py
+++ tests/registryd/test_registry_startup.py
@@ -1,6 +1,14 @@
 PROPERTIES_IFACE = 'org.freedesktop.DBus.Properties'
 ACCESSIBLE_IFACE = 'org.a11y.atspi.Accessible'
 
+def get_property(proxy, iface_name, prop_name):
+    return proxy.Get(iface_name, prop_name, dbus_interface=PROPERTIES_IFACE)
+
 def test_accessible_iface_properties(registry, session_manager):
-    val = registry.Get(ACCESSIBLE_IFACE, 'Name', dbus_interface=PROPERTIES_IFACE)
-    assert str(val) == 'main'
+    values = [
+        ('Name',        'main'),
+        ('Description', ''),
+    ]
+
+    for prop_name, expected in values:
+        assert get_property(registry, ACCESSIBLE_IFACE, prop_name) == expected
