e160239b7be6bced6f82fdd862c354a9d4847151
Federico Mena Quintero <federico@gnome.org>
2023-05-09 11:23:10 -0600

    atspi-misc.c: Remove debug g_print

--- atspi/atspi-misc.c
+++ atspi/atspi-misc.c
@@ -246,8 +246,6 @@ get_application (const char *bus_name)
   DBusMessage *message;
   DBusPendingCall *pending = NULL;
 
-  g_print ("get_application %s\n", bus_name);
-
   if (!app_hash)
     {
       app_hash = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, (GDestroyNotify) g_object_unref);
