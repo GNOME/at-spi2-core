113d5b0400475adacf3519cbf634944751e8c069
Lukáš Tyrychtr <ltyrycht@redhat.com>
2023-06-01 17:21:19 +0200

    ci: Generalize ci/run-style-check.sh a bit
    This:
    * Adds a remote only if necessary
    * Does not assume that we're running in CI, so it can be run locally with a sensible result

--- ci/run-style-check.sh
+++ ci/run-style-check.sh
@@ -2,16 +2,18 @@
 
 set -e
 
-# We need to add a new remote for the upstream main, since this script could
+# We might need to add a new remote for the upstream main, since this script could
 # be running in a personal fork of the repository which has out of date branches.
-if [ "${CI_PROJECT_NAMESPACE}" != "GNOME" ]; then
+UPSTREAM_URL=https://gitlab.gnome.org/GNOME/at-spi2-core.git
+UPSTREAM_REMOTE=$(git remote -v | awk -v url="$UPSTREAM_URL" '$2 == url { print $1; exit }')
+if [ -n "$UPSTREAM_REMOTE" ]; then
+    echo "Reusing the existing repository on ${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}, it already has a remote, ${UPSTREAM_REMOTE}"
+    ORIGIN="$UPSTREAM_REMOTE"
+else
     echo "Retrieving the current upstream repository from ${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}..."
     git remote add upstream https://gitlab.gnome.org/GNOME/at-spi2-core.git
     git fetch upstream
     ORIGIN="upstream"
-else
-    echo "Reusing the existing repository on ${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}"
-    ORIGIN="origin"
 fi
 
 # Wrap everything in a subshell so we can propagate the exit status.
@@ -23,7 +25,7 @@ fi
 #
 # `${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}` is only defined if we’re running in
 # a merge request pipeline; fall back to `${CI_DEFAULT_BRANCH}` otherwise.
-newest_common_ancestor_sha=$(diff --old-line-format='' --new-line-format='' <(git rev-list --first-parent "${ORIGIN}/${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-${CI_DEFAULT_BRANCH}}") <(git rev-list --first-parent HEAD) | head -1)
+newest_common_ancestor_sha=$(diff --old-line-format='' --new-line-format='' <(git rev-list --first-parent "${ORIGIN}/${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-${CI_DEFAULT_BRANCH:-main}}") <(git rev-list --first-parent HEAD) | head -1)
 git diff -U0 --no-color "${newest_common_ancestor_sha}" | clang-format-diff -p1 > format-diff.log
 )
 exit_status=$?
