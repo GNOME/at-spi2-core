69b5ecd574d4380a3f4c7eab34c990de60f7a497
Federico Mena Quintero <federico@gnome.org>
2023-06-28 10:37:21 -0600

    registry.c: return_v_string(): just g_error() if we run out of memory
    dbus_message_iter_open_container() returns FALSE on OOM.  However, the
    rest of the code is not equipped to deal with this (what would it do -
    sleep a bit and retry to send the reply!?), and the rest of glib
    already has the convention of aborting on OOM via g_error().

--- registryd/registry.c
+++ registryd/registry.c
@@ -105,18 +105,19 @@ spi_registry_init (SpiRegistry *registry)
 
 /*---------------------------------------------------------------------------*/
 
-static dbus_bool_t
+static void
 return_v_string (DBusMessageIter *iter, const gchar *str)
 {
   DBusMessageIter variant;
 
   if (!dbus_message_iter_open_container (iter, DBUS_TYPE_VARIANT, "s",
                                          &variant))
-    return FALSE;
+    {
+      g_error ("Out of memory");
+    }
 
   dbus_message_iter_append_basic (&variant, DBUS_TYPE_STRING, &str);
   dbus_message_iter_close_container (iter, &variant);
-  return TRUE;
 }
 
 static dbus_bool_t
@@ -615,18 +616,18 @@ impl_GetAlpha (DBusMessage *message, SpiRegistry *registry)
 /* org.at_spi.Accessible interface */
 /*---------------------------------------------------------------------------*/
 
-static dbus_bool_t
+static void
 impl_get_Name (DBusMessageIter *iter, SpiRegistry *registry)
 {
   const gchar *name = "main";
-  return return_v_string (iter, name);
+  return_v_string (iter, name);
 }
 
-static dbus_bool_t
+static void
 impl_get_Description (DBusMessageIter *iter, SpiRegistry *registry)
 {
   const gchar *description = "";
-  return return_v_string (iter, description);
+  return_v_string (iter, description);
 }
 
 static dbus_bool_t
@@ -658,16 +659,16 @@ impl_get_ChildCount (DBusMessageIter *iter, SpiRegistry *registry)
   return result;
 }
 
-static dbus_bool_t
+static void
 impl_get_ToolkitName (DBusMessageIter *iter, SpiRegistry *registry)
 {
-  return return_v_string (iter, "at-spi-registry");
+  return_v_string (iter, "at-spi-registry");
 }
 
-static dbus_bool_t
+static void
 impl_get_ToolkitVersion (DBusMessageIter *iter, SpiRegistry *registry)
 {
-  return return_v_string (iter, "2.0");
+  return_v_string (iter, "2.0");
 }
 
 static DBusMessage *
