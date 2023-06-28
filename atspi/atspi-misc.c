c0856f731f40a22113a7ade250f16cbc0fc6e141
Federico Mena Quintero <federico@gnome.org>
2023-06-27 20:18:12 -0600

    Remove superfluous check - g_hash_table_new_full() can't fail

--- atspi/atspi-misc.c
+++ atspi/atspi-misc.c
@@ -408,8 +408,6 @@ get_application (const char *bus_name)
   if (!app_hash)
     {
       app_hash = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, (GDestroyNotify) g_object_unref);
-      if (!app_hash)
-        return NULL;
     }
   app = g_hash_table_lookup (app_hash, bus_name);
   if (app)
