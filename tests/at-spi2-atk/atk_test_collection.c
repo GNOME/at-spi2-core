7d29783b421718786ac6db679adcd4738e72a0dd
Mike Gorse <mgorse@suse.com>
2023-07-19 12:08:01 -0500

    collection: Fix match testing for attributes
    The predicates for ATSPI_COLLECTION_MATCH_TYPE_NONE and
    ATSPI_COLLECTION_MATCH_TYPE_ALL were both broken.
    
    Fixes #127

--- tests/at-spi2-atk/atk_test_collection.c
+++ tests/at-spi2-atk/atk_test_collection.c
@@ -117,6 +117,7 @@ atk_test_collection_get_matches_from (gpointer fixture, gconstpointer user_data)
 {
   AtspiAccessible *obj = get_root_obj (DATA_FILE);
   AtspiCollection *iface = atspi_accessible_get_collection_iface (obj);
+  GHashTable *attributes;
   g_assert (iface);
 
   AtspiAccessible *child = atspi_accessible_get_child_at_index (obj, 0, NULL);
@@ -150,6 +151,36 @@ atk_test_collection_get_matches_from (gpointer fixture, gconstpointer user_data)
   g_assert_cmpstr ("obj3", ==, atspi_accessible_get_name (get, NULL));
   get = g_array_index (ret, AtspiAccessible *, 2);
   g_assert_cmpstr ("obj3", ==, atspi_accessible_get_name (get, NULL));
+  g_array_free (ret, TRUE);
+  g_object_unref (rule);
+
+  attributes = g_hash_table_new (g_str_hash, g_str_equal);
+  g_hash_table_insert (attributes, "layout-guess", "true");
+  rule = atspi_match_rule_new (NULL,
+                               ATSPI_Collection_MATCH_ALL,
+                               attributes,
+                               ATSPI_Collection_MATCH_NONE,
+                               NULL,
+                               ATSPI_Collection_MATCH_ALL,
+                               NULL,
+                               ATSPI_Collection_MATCH_ALL,
+                               FALSE);
+  ret = atspi_collection_get_matches_from (iface,
+                                                   child1,
+                                                   rule,
+                                                   ATSPI_Collection_SORT_ORDER_CANONICAL,
+                                                   ATSPI_Collection_TREE_INORDER,
+                                                   0,
+                                                   FALSE,
+                                                   NULL);
+  g_hash_table_unref (attributes);
+  g_assert_cmpint (6, ==, ret->len);
+  g_array_free (ret, TRUE);
+  g_object_unref (rule);
+
+  g_object_unref (child1);
+  g_object_unref (child);
+  g_object_unref (iface);
 }
 
 void
